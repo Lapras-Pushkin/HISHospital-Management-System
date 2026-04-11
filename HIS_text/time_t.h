f012819da1c3d7bc9b4c1edc05c0d175b18a20d1
Lapras_pushkin <@LAPTOP-H1URKEJP>
2026-04-11 18:37:42 +0800

    chen

--- HIS_text/time_t.h
+++ HIS_text/time_t.h
@@ -1,12 +1,11 @@
 #pragma once
-#ifndef TIME_T_H
-#define TIME_T_H
+#ifndef UTILS_H
+#define UTILS_H
 
 #include <time.h>
-#include <stddef.h>
 
-void getCurrentTime(char* buffer, size_t size);
-void getCurrentDate(char* buffer, size_t size);
+void getCurrentTime(char* buffer, int size);
+void getCurrentDate(char* buffer, int size);
 void getPastDateAccurate(char* current, char* result, int days_ago);
 void judgetime(char* end);
 int isLeapYear(int year);
